4ee8d6cf7f37d7bc5cab708a5835557c7936ba8b
techpro <mehmetqa.techproed@gmail.com>
2023-07-05 13:57:46 +0300

    day34lambda

--- src/main/java/day33exceptionsenumsiterator/ExceptionRunner.java
+++ src/main/java/day33exceptionsenumsiterator/ExceptionRunner.java
@@ -3,45 +3,49 @@ package day33exceptionsenumsiterator;
 public class ExceptionRunner {
 
     /*
-    Garbage Collector memory i temiz tutmak icin surekli memory i tarar ve silinmesi gerekenleri siler
-    Garbagec Collector silmeden once silecegi datalari finalize eder ve sonra siler
+    Garbage Collector memory i temiz tutmak icin surekli memoryi tarar ve silinmesi gerekenleri siler
+    Garbage Collector silmeden once silecegi datalari finalize eder sonra siler
      */
 
 
     public static void main(String[] args) throws InvalidStudentGradeException {
 
         getStudentGrade(50);
-      //  getStudentGrade(150); //InvalidStudentGradeException
+      //  getStudentGrade(-50);//InvalidStudentGradeException
 
-        getTheNumberOfStudents(15);
-        getTheNumberOfStudents(-15);
+        getTheNumberOfStudents(50);//50
+        getTheNumberOfStudents(-50);//InvalidNumberException
+
+
+
+
+
 
     }
-    public static void getTheNumberOfStudents(int numOfStudens){
+    public static void getTheNumberOfStudents(int numOfStudents){
 
-        if(numOfStudens<0){
-            try{
+        if(numOfStudents<0){
+            try {
                 throw new InvalidNumberException("Student number cannot be negative");
-            }catch (InvalidNumberException e){
+            } catch (InvalidNumberException e) {
                 System.err.println(e.getMessage());
             }
-
         }else{
-            System.out.println(numOfStudens);
+            System.out.println(numOfStudents);
         }
     }
-
 
 
     public static void getStudentGrade(int grade) throws InvalidStudentGradeException {
 
-        if(grade<0 || grade >100 ){
+        if(grade <0 || grade>100){
             throw new InvalidStudentGradeException("Student's grades cannot be less than zero or greater than 100");
-        }else{
+        }else {
             System.out.println(grade);
         }
 
-
     }
+
+
 
 }
--- src/main/java/day33exceptionsenumsiterator/Iterators01.java
+++ src/main/java/day33exceptionsenumsiterator/Iterators01.java
@@ -8,91 +8,88 @@ import java.util.ListIterator;
 public class Iterators01 {
     public static void main(String[] args) {
         /*
-        1) Iterator' lar looplarin yaptigi ayni isi yapar
-        2) Iterator'larda sonsus loop olusma ihtimali yoktur
-        3) Iterator'lar ile loop'lar arasinda performans farki yoktur.
-        4) Iterator'lar bir collection'dan eleman silme ve bir collection daki elemani degistirme konusunda daha basarilidir
-        5) Iki tip iterator vardir;
-            a) Iterator : Bu sadece eleman silmede kullanilir
-                          Eleman eklemek veya elemani degistirmek mumkun degildir
+        1) Iterator'lar loop larin yaptigi ayni isi yapar
+        2) Iterator' larda sonsuz loop olusma ihtimali yoktur
+        3) Iterator'lar ile looplar arasinda performans farki yoktur
+        4) Iterator'lar bir collection dan eleman silme ve bir collection daki elemani degistirme konusunda daha basarilidir
+        5) Iki tip iterator var;
+            a) Iterator : Bu sadece eleman silmede kullanilir.
+                          Eleman eklemek veya elemani degistirmek mumkun degildir.
 
-            b) ListIterator: Bu eleman silebilir, ekleyebilir ve degistirebilir.
+            b)ListIterator: Bu eleman silebililr, ekleyebilir ve degistirebilir
 
-            Note: "Iterator" sadece soldan saga (ilk elemandan son elemana) calisir
-                   "ListIterator" ise iki yonlu calisabilir
+            Note: iterator sadece soldan saga (ilk elemandan son elemana) calisir
+                  ListIterator ise iki yonlu calisabilir.
          */
 
+
         List<String> myList = new ArrayList<>();
-        myList.add("Emin");
-        myList.add("Bugra");
-        myList.add("Hasan");
-        myList.add("Benna");
-        myList.add("Ali");
-        System.out.println(myList);//[Emin, Bugra, Hasan, Benna, Ali]
+        myList.add("Niyazi");
+        myList.add("Saadet");
+        myList.add("Fevzi");
+        myList.add("Suat");
+        myList.add("Mustafa");
+        System.out.println(myList);//[Niyazi, Saadet, Fevzi, Suat, Mustafa]
 
 
-        //itarator kullanarak specific bir eleman nasil silinir?
-        Iterator<String> myItr = myList.iterator();//[  Emin,     Bugra,   Hasan,     Benna,   Ali  ]
+        Iterator<String> myItr = myList.iterator();//[  Niyazi,     Saadet,     Fevzi,  Suat,   Mustafa ]
+
+        //iterator ile specific bir eleman nasil silinir?
 
         while (myItr.hasNext()) {//hasNext() pointer a senden sonra eleman var mi? diye sorar.
-
-            //Elemean varsa true yoksa false return eder
-            String el = myItr.next();   //next() pointer i bir sonraki elemanin onune tasir ve ustunden atladigi elemani return eder
-
-            if (el.equals("Ali")) {
-                myItr.remove(); //next() methodunun return ettigi elemani siler
+            //Eleman varsa true yoksa false return eder
+            String el = myItr.next();   //next(); pointeri bir sonraki elemanin onune tasir ve ustunden atladigi elemani return eder
+            if (el.equals("Niyazi")) {
+                myItr.remove(); //next(); methodunun return ettigi elemani siler
             }
-
         }
-        System.out.println(myList);//[]
+        System.out.println(myList);
 
         //ListIterator kullanimi
         List<String> yourList = new ArrayList<>();
-        yourList.add("Hasan");
-        yourList.add("Benna");
-        yourList.add("Ali");
-        System.out.println(yourList);//[Hasan, Benna, Ali]
+        yourList.add("Niyazi");
+        yourList.add("Saadet");
+        yourList.add("Fevzi");
+        System.out.println(yourList);//[Niyazi, Saadet, Fevzi]
 
-        ListIterator<String> yourListItr = yourList.listIterator();//[  Hasan,     Benna,    Ali ]
+        ListIterator<String> yourListItr = yourList.listIterator();//[      Niyazi,      Saadet,         Fevzi]
 
         while (yourListItr.hasNext()) {
             String el = yourListItr.next();
-            yourListItr.set(el + "*");//set () methodu listi update etmeye yarar
+            yourListItr.set(el + "*");//set() methodu listi update islemi icin kullanilir
         }
-        System.out.println(yourList);
+        System.out.println(yourList);//[Niyazi*, Saadet*, Fevzi*]
 
 
-        //previous method kullanimi
+        //ListIterator previous method kullanimi
         List<String> list3 = new ArrayList<>();
-        list3.add("Hasan");
-        list3.add("Benna");
-        list3.add("Ali");
-        System.out.println(list3);//[ Hasan, Benna, Ali]
+        list3.add("Niyazi");
+        list3.add("Saadet");
+        list3.add("Fevzi");
+        System.out.println(list3);//[Niyazi, Saadet, Fevzi]
 
-        ListIterator<String> listItr = list3.listIterator();//[ Hasan, Benna, Ali]
+        ListIterator<String> listItr=  list3.listIterator();//[  Niyazi,     Saadet,     Fevzi ]
 
-        //pointer i en saga almak icin
+        //Loop kullanarak pointeri en sona almaliliyiz
         while (listItr.hasNext()){
             listItr.next();
         }
-        //pointeri yukaridaki kod ile ensonda biraktigimiz icin
-        // elemanlari en sondan basa dogru yazdirabiliriz
-
 
         while (listItr.hasPrevious()){
-
-           String el= listItr.previous();
-
-
-
-
-
-
-
-            System.out.println(el+ "<===");
-
+           String el = listItr.previous();
+            System.out.println(el+"<==");
 
         }
+
+
+
+
+
+
+
+
+
+
 
 
 
